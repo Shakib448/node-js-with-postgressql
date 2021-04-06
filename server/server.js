309ab435fd2336039723b48440d56f239776c826
Shakiba448 <shakiba448@gmail.com>
2021-04-06 09:22:48 +0600

    Node js with PostgresSQL

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import "colors";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Body parser
+app.use(express.json());
+
+// App routes
+// app.use("/api/employee", EmployeeRoutes);
+
+app.listen(
+  PORT,
+  console.log(`Server is running on http://localhost:${PORT}`.yellow.bold)
+);
