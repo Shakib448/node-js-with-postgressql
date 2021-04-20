d4a5d16e5efdc1be0ea17c359684f62df80b72a7
Shakiba448 <shakiba448@gmail.com>
2021-04-20 16:07:16 +0600

    WIP on Royal Home

--- client/src/Components/Common/Banner/Banner.js
+++ client/src/Components/Common/Banner/Banner.js
@@ -2,7 +2,11 @@ import { Grid } from "@material-ui/core";
 import clsx from "clsx";
 import style from "./Banner.module.scss";
 const Banner = () => {
-  return <Grid container className={clsx(style.banner__bg)}></Grid>;
+  return (
+    <section className={clsx(style.banner__area)}>
+      <Grid container className={clsx(style.banner__bg)}></Grid>
+    </section>
+  );
 };
 
 export default Banner;
