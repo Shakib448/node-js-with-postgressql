48a742236357a3a4df76763295263e38844076b9
Shakiba448 <shakiba448@gmail.com>
2021-04-26 13:56:45 +0600

    WIP on Home

--- client/src/Components/Common/Footer/Footer.js
+++ client/src/Components/Common/Footer/Footer.js
@@ -1,13 +1,28 @@
-import { Container, Grid } from "@material-ui/core";
+import { Container, makeStyles, Grid } from "@material-ui/core";
 import React from "react";
+import clsx from "clsx";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: "#04091E",
+    padding: theme.spacing(3),
+    margin: `${theme.spacing(3)}px 0px 0px 0px`,
+    height: "200px",
+  },
+}));
 
 const Footer = () => {
+  const classes = useStyles();
   return (
-    <Grid container>
+    <footer className={clsx(classes.root)}>
       <Container>
-        <h1>This is footer</h1>
+        <Grid container>
+          <Grid item md={3} lg={3} sm={12}></Grid>
+          <Grid item md={3} lg={3} sm={12}></Grid>
+          <Grid item md={3} lg={3} sm={12}></Grid>
+        </Grid>
       </Container>
-    </Grid>
+    </footer>
   );
 };
 
