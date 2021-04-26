4303f554e432c0371d57f01696e11780b43c3626
Shakiba448 <shakiba448@gmail.com>
2021-04-26 14:24:02 +0600

    WIP on Home

--- client/src/Components/Common/Footer/Footer.js
+++ client/src/Components/Common/Footer/Footer.js
@@ -11,8 +11,11 @@ import clsx from "clsx";
 const useStyles = makeStyles((theme) => ({
   root: {
     background: "#04091E",
-    margin: `${theme.spacing(3)}px 0px 0px 0px`,
+    margin: `${theme.spacing(4)}px 0px 0px 0px`,
     padding: theme.spacing(4),
+  },
+  content: {
+    padding: `${theme.spacing(4)}px 0px`,
   },
 }));
 
@@ -20,30 +23,41 @@ const Footer = () => {
   const classes = useStyles();
   return (
     <footer className={clsx(classes.root)}>
-      <Container>
-        <Grid container justify="center">
-          <Grid item md={3} lg={3} sm={12}>
-            <Grid container direction="column">
-              <Typography variant="h6" style={{ color: "white" }}>
-                ABOUT AGENCY
-              </Typography>
-              <Typography
-                variant="subtitle2"
-                style={{ color: "#727272", fontWeight: "300" }}
-              >
-                <Box mt={2}>
-                  The world has become so fast paced that people don’t want to
-                  stand by reading a page of information, they would much rather
-                  look at a presentation and understand the message. It has come
-                  to a point
-                </Box>
-              </Typography>
-            </Grid>
+      <Grid
+        container
+        justify="center"
+        spacing={2}
+        className={clsx(classes.content)}
+      >
+        <Grid item md={3} lg={3} sm={12}>
+          <Grid container direction="column">
+            <Typography variant="h6" style={{ color: "white" }}>
+              ABOUT AGENCY
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              style={{ color: "#727272", fontWeight: "300" }}
+            >
+              <Box mt={2}>
+                The world has become so fast paced that people don’t want to
+                stand by reading a page of information, they would much rather
+                look at a presentation and understand the message. It has come
+                to a point
+              </Box>
+            </Typography>
           </Grid>
-          <Grid item md={3} lg={3} sm={12}></Grid>
-          <Grid item md={3} lg={3} sm={12}></Grid>
         </Grid>
-      </Container>
+        <Grid item md={3} lg={3} sm={12}>
+          <Typography variant="h6" style={{ color: "white" }}>
+            NAVIGATION LINK
+          </Typography>
+          <Box mt={2}>
+            <Grid container md={6} lg={6} sm={12}></Grid>
+            <Grid container md={6} lg={6} sm={12}></Grid>
+          </Box>
+        </Grid>
+        <Grid item md={3} lg={3} sm={12}></Grid>
+      </Grid>
     </footer>
   );
 };
