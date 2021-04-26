15bcaf330039b70491b0d437f2bda729ce3ad778
Shakiba448 <shakiba448@gmail.com>
2021-04-26 13:46:59 +0600

    WIP on Home

---
+++ client/src/Components/Common/Footer/Footer.js
@@ -0,0 +1,14 @@
+import { Container, Grid } from "@material-ui/core";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <Grid container>
+      <Container>
+        <h1>This is footer</h1>
+      </Container>
+    </Grid>
+  );
+};
+
+export default Footer;
