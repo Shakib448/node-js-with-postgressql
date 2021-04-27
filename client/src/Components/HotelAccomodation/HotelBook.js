54077477620a31df644e3791368fe609aae5ef5f
Shakiba448 <shakiba448@gmail.com>
2021-04-28 02:57:38 +0600

    WIP on Royal Home

--- client/src/Components/HotelAccomodation/HotelBook.js
+++ client/src/Components/HotelAccomodation/HotelBook.js
@@ -5,18 +5,24 @@ import clsx from "clsx";
 const useStyles = makeStyles((theme) => ({
   media: {
     cursor: "pointer",
-    position: "relative",
     "&:hover": {
       transform: "scale(1.2)",
     },
+  },
+  grid: {
+    position: "relative",
   },
   btn: {
     background: "#F2C201",
     padding: `${theme.spacing(1)}px ${theme.spacing(4)}px`,
     fontSize: "14px",
     color: "#fff",
+    boxShadow: "none",
+    position: "absolute",
+    bottom: 20,
     "&:hover": {
       background: "#F2C201",
+      boxShadow: "none",
     },
   },
 }));
@@ -25,19 +31,24 @@ const HotelBook = () => {
   const classes = useStyles();
   return (
     <Grid container direction="row" justify="center">
-      <Grid item container md={3} lg={3} sm={12} xs={12} justify="center">
+      <Grid
+        item
+        container
+        md={3}
+        lg={3}
+        sm={12}
+        xs={12}
+        justify="center"
+        className={clsx(classes.grid)}
+      >
         <Image
           className={classes.media}
           src="/image/room1.jpg"
           title="Room1 image"
           width={250}
-          height={250}
+          height={300}
         />
-        <Button
-          variant="contained"
-          color="primary"
-          className={clsx(classes.btn)}
-        >
+        <Button variant="contained" className={clsx(classes.btn)}>
           BOOK NOW
         </Button>
       </Grid>
